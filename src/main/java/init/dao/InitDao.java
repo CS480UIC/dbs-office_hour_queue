4e0ebe9af6c80bfe211f996c7ed8fc7068977661
ddanciu2 <ddanci2@uic.edu>
2022-04-01 12:07:57 -0500

    Update initializeDB for front end/sql script

---
+++ src/main/java/init/dao/InitDao.java
@@ -0,0 +1,61 @@
+package init.dao;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import user.domain.User;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+
+
+/**
+ * DDL functions performed in database
+ */
+public class InitDao {
+	
+	/**
+	 * user name to connect to the database 
+	 */
+	private String MySQL_user = "office_hour_queue";  //TODO change user
+	
+	/**
+	 * password of your username to connect to the database
+	 */
+	private String MySQL_password = "@EzPass#";  //TODO change password
+	
+
+	
+
+	
+	public void initialize(InputStream input) throws ClassNotFoundException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306", MySQL_user, MySQL_password);
+//			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/office_hour_queue", MySQL_user, MySQL_password);
+			
+			//Initialize the script runner
+	   		 ScriptRunner sr = new ScriptRunner(connect);
+	   		 //Creating a reader object
+	   		 Reader reader = new InputStreamReader(input);
+	   		 //Running the script
+	   		 sr.runScript(reader);
+						
+		    connect.close();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+		
+}
---
+++ src/main/java/init/service/InitService.java
@@ -0,0 +1,24 @@
+package init.service;
+
+import java.io.InputStream;
+
+import init.dao.InitDao;
+
+/**
+ * logic functions such as register, login
+ * @author Aayush Makharia
+ */
+public class InitService {
+	private InitDao initDao = new InitDao();
+	public Object initializeDB;
+	
+	/**
+	 * Register a user
+	 * @param input 
+	 */
+	public void initializeDB(InputStream input) throws InitException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+		initDao.initialize(input);
+	}
+	
+
+}
